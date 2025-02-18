86ed4e4ba1515e9cddf7e186aecadf5afef0c5a1
Tadashi <tadasho@gmail.com>
2025-02-18 18:19:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
 docker pull universalrobots/ursim_cb3
+
+# Navigate to the directory containing setup.py
+cd old-ds-opt-py
+
+# Install the package globally in editable mode using pip
+sudo pip3 install -e .
